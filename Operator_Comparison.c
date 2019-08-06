3ca3de40c47259e200cdc24099dc3c7c8e76a40b
Prashik Deshpande <43670200+PrashikD@users.noreply.github.com>
2019-08-06 16:35:56 +0530

    Updated the code for good
    Added more ways and minimized the code

--- Operator_Comparison.c
+++ Operator_Comparison.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    unsigned long int i=0;
+    unsigned long int i;
     clock_t start_time = clock();
     for(i=0;i<1000000000;){
         i++;
@@ -11,13 +11,28 @@ int main()
     clock_t lap_1 = clock();
     double time_1 = ((double)(lap_1 - start_time))/CLOCKS_PER_SEC;
     printf("%f\n",time_1);
-    i = 0;
+    
     for(i=0;i<1000000000;){
         i = i + 1;
     }
     clock_t lap_2 = clock();
     double time_2 = ((double)(lap_2 - lap_1))/CLOCKS_PER_SEC;
     printf("%f\n",time_2);
+    
+    for(i=0;i<1000000000;){
+        ++i;
+    }
+    clock_t lap_3 = clock();
+    double time_3 = ((double)(lap_3 - lap_2))/CLOCKS_PER_SEC;
+    printf("%f\n",time_3);
+    
+    for(i=0;i<1000000000;){
+        i += 1;
+    }
+    clock_t lap_4 = clock();
+    double time_4 = ((double)(lap_4 - lap_3))/CLOCKS_PER_SEC;
+    printf("%f\n",time_4);
+    
     
     return 1;
 }
